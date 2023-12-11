21a30541bc3a62742d863ab09bf7a28b03f21be8
EHoTiNKA <alexe.pyshkin@yandex.ru>
2023-12-11 13:32:16 +0300

    add_JSON_Questions

--- models.py
+++ models.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class User(BaseModel):
     name: str or None = None
     group: int or None = None
+
+class Question(BaseModel):
+    text: str
+    answers: list[str]
+    correct_answer_id: int
--- quiz.py
+++ quiz.py
@@ -2,22 +2,62 @@ import json
 from aiogram.enums import ParseMode
 from aiogram.methods import send_poll
 from aiogram.types.poll_answer import PollAnswer
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 from aiogram import types, Router
 from aiogram.filters import Command
+
+from models import Question
 
 
 router = Router(name=__name__)
 
+with open('questions.json', 'r') as file:
+    quiz_data = json.load(file)
+    questions = [Question(**question) for question in quiz_data]
+
+class QuizState(StatesGroup):
+    quiz = State()
 
 @router.message(Command("startquiz"))
-async def start_quiz(message: types.Message):
-    with open('questions.json', 'r') as file:
-        questions = json.load(file)
+async def start_quiz(message: types.Message, state: FSMContext):
 
-    await router.send_poll(
-        chat_id=message.chat.id,
-        question="Сколько будет 2 + 2",
-        options=["3", "4", "1"],
-        correct_option_id=1,
+    await state.set_state(QuizState.quiz)
+    await state.update_data(quiz={'question_id': 0})
+
+    question = questions[0]
+
+    await message.answer_poll(
+        question=question.text,
+        options=question.answers,
+        correct_option_id=question.correct_answer_id,
         is_anonymous=False,
     )
+
+# @router.poll_answer_handler(state=QuizState.quiz)
+# async def handle_poll_answer(poll_answer: PollAnswer, state: FSMContext):
+#     user_id = poll_answer.user.id
+#     selected_option_id = poll_answer.option_ids[0]  # предполагаем, что только один ответ может быть выбран
+
+#     # Ваш код для обработки ответа на опрос здесь
+#     # Можете использовать state для хранения данных и следующего вопроса
+#     data = await state.get_data()
+#     current_question_id = data['quiz']['question_id']
+
+#     # Проверяем, если есть следующий вопрос
+#     if current_question_id + 1 < len(questions):
+#         next_question_id = current_question_id + 1
+#         await state.update_data(quiz={'question_id': next_question_id})
+#         next_question = questions[next_question_id]
+
+#         # Отправляем следующий вопрос
+#         await poll_answer.answer_poll(
+#             chat_id=user_id,
+#             question=next_question.text,
+#             options=next_question.answers,
+#             correct_option_id=next_question.correct_answer_id,
+#             is_anonymous=False,
+#         )
+#     else:
+#         # Если вопросы закончились, завершаем состояние
+#         await state.finish()
